b3491a87c0503d3ffd172ff6da45d722abf15624
DFuh <dafu@uni-bremen.de>
2020-05-07 21:07:09 +0200

    edit basic structure

---
+++ elPreDator/epr_run.py
@@ -0,0 +1,22 @@
+'''
+main script for running the application
+'''
+
+from processdata.aux import hlfiles as hlf
+
+
+class dataInstance():
+
+    def __init__():
+        pass
+
+    
+
+
+def main_fun():
+
+    pass
+
+
+if __name__ == '__main__':
+    main_fun()
