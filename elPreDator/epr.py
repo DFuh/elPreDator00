b980e5484e336004e6c2d6aa140c6aad611a6569
DFuh <dafu@uni-bremen.de>
2020-05-27 14:49:47 +0200

    -- further structuring -- include existing aux. functions

---
+++ elPreDator/epr.py
@@ -0,0 +1,24 @@
+'''
+make data-instances and ctrl processing
+'''
+from aux import hlfiles as hlf
+from aux import prcssdata as pcd
+
+
+
+
+
+### define class inside mainfun, or import class in "run.py" ????
+
+def main_fun():
+    ## read par from json-file
+    with open(pth+'/testfile_02.json') as json_data:
+        data = json.load(json_data)
+
+    
+    class dataInstance():
+
+        def __init__():
+            pass
+
+    pass
---
+++ elPreDator/run.py
@@ -0,0 +1,14 @@
+'''
+main script for running the application
+'''
+
+import epr
+
+### path input
+
+### 
+
+
+
+if __name__ == '__main__':
+    epr.main_fun()
